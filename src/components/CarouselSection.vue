f6e2117fff4c06a34b48c60dbd3de049e5fba971
a-lotysh <andriylotish1@gmail.com>
2024-06-19 00:43:32 +0300

    add carousel

--- src/components/CarouselSection.vue
+++ src/components/CarouselSection.vue
@@ -1,62 +1,39 @@
 <script setup lang="ts">
 import { Carousel3d, Slide } from '@nanoandrew4/vue3-carousel-3d';
-import { reactive } from 'vue';
-
-const slides = reactive([
-  {
-    id: 0,
-    imageUrl: 'src/assets/carousel/01.jpg',
-  },
-  {
-    id: 1,
-    imageUrl: 'src/assets/carousel/02.jpg',
-  },
-  {
-    id: 2,
-    imageUrl: 'src/assets/carousel/03.jpg',
-  },
-  {
-    id: 3,
-    imageUrl: 'src/assets/carousel/04.jpg',
-  },
-  {
-    id: 4,
-    imageUrl: 'src/assets/carousel/05.jpg',
-  },
-  {
-    id: 5,
-    imageUrl: 'src/assets/carousel/06.jpg',
-  },
-  {
-    id: 6,
-    imageUrl: 'src/assets/carousel/07.jpg',
-  },
-  {
-    id: 7,
-    imageUrl: 'src/assets/carousel/08.jpg',
-  },
-  {
-    id: 8,
-    imageUrl: 'src/assets/carousel/09.jpg',
-  },
-  {
-    id: 9,
-    imageUrl: 'src/assets/carousel/10.jpg',
-  },
-]);
 </script>
 
 <template>
   <div id="carousel">
     <Carousel3d :autoplay="true" :autoplay-timeout="2000">
-      <Slide
-          class="carousel-slide"
-          v-for="slide in slides"
-          :index="slide.id"
-          :key="slide.id"
-          style="border-width: 0; width: 200px"
-      >
-        <img class="slide-image" :src="slide.imageUrl" alt="Image">
+      <Slide class="carousel-slide" :index="0" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/01.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="1" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/02.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="2" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/03.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="3" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/04.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="4" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/05.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="5" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/06.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="6" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/07.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="7" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/08.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="8" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/09.jpg" alt="Image">
+      </Slide>
+      <Slide class="carousel-slide" :index="9" style="border-width: 0; width: 200px">
+        <img class="slide-image" src="../assets/carousel/10.jpg" alt="Image">
       </Slide>
     </Carousel3d>
   </div>
